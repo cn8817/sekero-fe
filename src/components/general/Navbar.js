e3dcdf1a8907bc7d084db740b360d76b0b720c0d
cn8817 <christinenguyen1433@gmail.com>
2021-12-31 23:27:45 -0800

    created the navbar structure and styled it

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { ShoppingCartIcon } from '@heroicons/react/outline'
 
 export default function Navbar(){
     return(
-        <div>
-            
+        <div className='navbar-container'>
+            <div className='navbar-logo'>
+                <h1>LOGO</h1>
+            </div>
+            <div>
+                <ul className='navbar-items'>
+                    <li>HOME</li>
+                    <li>OUR STORY</li>
+                    <li>SHOP</li>
+                    <li>RECIPES</li>
+                    <li className='navbar-cart'><ShoppingCartIcon className="h-5 w-5 text-gray-500"/>CART</li>
+                </ul>
+            </div>
         </div>
     )
 }
