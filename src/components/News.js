619345eabb1b4c1ca8bb8f48a0edd8ddde2959f1
cn8817 <christinenguyen1433@gmail.com>
2022-01-03 17:14:01 -0800

    finished the structure of the news subcription component

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,14 @@
+import Button from "./Button"
+
 export default function News(){
     return(
-        <div>
-            
+        <div className='flex flex-col items-center bg-base2 mb-10 h-[55vh]'>
+            <p className='text-highlight pt-24 pb-3 uppercase small'>latest news</p>
+            <h2 className='pb-10'>Latest News & Updates</h2>
+            <div>
+                <input type='text' placeholder='Enter your email' className="rounded-full border-transparent focus:border-text focus:ring-0 w-96 pl-6"/>
+                <Button variant="secondary" className='ml-3'>subscribe</Button>
+            </div>
         </div>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,7 @@ import Hero from '../components/homepage/Hero'
 import PreviewProducts from '../components/homepage/PreviewProducts'
 import PreviewRecipies from '../components/homepage/PreviewRecipes'
 import Stores from '../components/homepage/Stores'
+import News from '../components/News'
 
 export default function HomePage(){
     return(
@@ -11,6 +12,7 @@ export default function HomePage(){
             <PreviewProducts/>
             <PreviewRecipies/>
             <Stores/>
+            <News/>
         </div>
     )
 }
