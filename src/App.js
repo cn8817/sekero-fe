f5ba49f0518307de346e0c927bb3345dd8c1b8e5
cn8817 <christinenguyen1433@gmail.com>
2022-01-03 17:30:57 -0800

    created and styled the footer structure

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Switch } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App m-0">
       <Navbar/>
       {/* <Button>button</Button>
       <Button variant='secondary'>secondary</Button> */}
       <Switch>
         <Route exact path='/' component={Homepage}/>
       </Switch>
+      <Footer/>
     </div>
   );
 }
--- src/components/News.js
+++ src/components/News.js
@@ -2,10 +2,10 @@ import Button from "./Button"
 
 export default function News(){
     return(
-        <div className='flex flex-col items-center bg-base2 mb-10 h-[55vh]'>
+        <div className='flex flex-col items-center bg-base2'>
             <p className='text-highlight pt-24 pb-3 uppercase small'>latest news</p>
-            <h2 className='pb-10'>Latest News & Updates</h2>
-            <div>
+            <h2 className='pb-20'>Latest News & Updates</h2>
+            <div className="pb-36">
                 <input type='text' placeholder='Enter your email' className="rounded-full border-transparent focus:border-text focus:ring-0 w-96 pl-6"/>
                 <Button variant="secondary" className='ml-3'>subscribe</Button>
             </div>
