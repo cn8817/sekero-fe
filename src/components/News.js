fc53f2079140d5a43dd161c2ab71e1ea4631d400
cn8817 <christinenguyen1433@gmail.com>
2022-01-03 17:35:40 -0800

    changed input type to email instead of text

--- src/components/News.js
+++ src/components/News.js
@@ -6,7 +6,7 @@ export default function News(){
             <p className='text-highlight pt-24 pb-3 uppercase small'>latest news</p>
             <h2 className='pb-20'>Latest News & Updates</h2>
             <div className="pb-36">
-                <input type='text' placeholder='Enter your email' className="rounded-full border-transparent focus:border-text focus:ring-0 w-96 pl-6"/>
+                <input type='email' placeholder='Enter your email' className="rounded-full border-transparent focus:border-text focus:ring-0 w-96 pl-6"/>
                 <Button variant="secondary" className='ml-3'>subscribe</Button>
             </div>
         </div>
