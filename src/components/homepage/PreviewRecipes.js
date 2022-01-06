a4db833b7679c672e2ace2cf1380927c22074fbc
cn8817 <christinenguyen1433@gmail.com>
2022-01-06 13:56:48 -0800

    implemented backend endpoint and displayed the data on the homepage

--- src/components/homepage/PreviewRecipes.js
+++ src/components/homepage/PreviewRecipes.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import Button from '../Button'
 import Tteokbokki from '../../assets/recipe-photo.png'
@@ -5,45 +7,52 @@ import Tteokguk from '../../assets/recipe-photo-2.png'
 
 export default function PreviewRecipies(){
 
-const dummyData = [
-    {
-        id: 1,
-        title: 'Tteokbokki',
-        alt: '(Spicy stir-fried Rice Caked)',
-        description: 'This popular street-food dish is a garlicky, richly spiced dish with cylindrical rice cakes bathed in red chili paste.',
-        picture: Tteokbokki,
-        servings: '2',
-        time: '20'
-    },
-    {
-        id: 1,
-        title: 'Tteokbokki',
-        alt: '(Spicy stir-fried Rice Caked)',
-        description: 'A traditional must-have holiday dish. Using rice cake coins, we share a recipe that warms the heart.',
-        picture: Tteokguk,
-        servings: '2',
-        time: '20'
-    },
-]
+// const dummyData = [
+//     {
+//         id: 1,
+//         title: 'Tteokbokki',
+//         alt: '(Spicy stir-fried Rice Caked)',
+//         description: 'This popular street-food dish is a garlicky, richly spiced dish with cylindrical rice cakes bathed in red chili paste.',
+//         picture: Tteokbokki,
+//         servings: '2',
+//         time: '20'
+//     },
+//     {
+//         id: 1,
+//         title: 'Tteokbokki',
+//         alt: '(Spicy stir-fried Rice Caked)',
+//         description: 'A traditional must-have holiday dish. Using rice cake coins, we share a recipe that warms the heart.',
+//         picture: Tteokguk,
+//         servings: '2',
+//         time: '20'
+//     },
+// ]
+
+const queryInfo = useQuery('recipes', () =>
+axios
+.get('https://43p44fmhh5.execute-api.us-west-1.amazonaws.com/dev/recipes')
+.then(res => res.data)
+)
+
     return(
         <div className="flex flex-col items-center mb-10 bg-base2 h-[100vh]">
              <p className='text-highlight pt-24 pb-3 uppercase small'>our secret korean recipes</p>
             <h2 className='pb-10'>Recipes</h2>
         <div className='container flex flex-row justify-between mb-10'>
-        {dummyData.map(item => {
+        {queryInfo.data?.slice(0,2).map(item => {
             return(
                     <div className='flex flex-row bg-base1 rounded-2xl overflow-hidden max-h-[18rem] max-w-[35rem]'>
                         <div>
-                            <img src={item.picture}/>
+                            <img src={item.image}/>
                         </div>
                         <div className='flex flex-col justify-between h-40'>
                             <div className='py-4 px-8'>
                                 <div className='font-bold pb-4'>
                                     <p>{item.title}</p>
-                                    <p>{item.alt}</p>
+                                    {/* <p>{item.alt}</p> */}
                                 </div>
-                                <p className='small pb-4'>{item.description}</p>
-                                <p className='small pb-4'>{item.servings} servings & {item.time} min total.</p>
+                                <p className='small pb-4'>{item.r_desc}</p>
+                                <p className='small pb-4'>{item.servings} servings & {item.minutes} min total.</p>
                                 <Button variant='secondary'>See Recipe</Button>
                         </div>
                     </div>
