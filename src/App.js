854f4b31efe8fd6167ba432d3638063064dd85c3
cn8817 <christinenguyen1433@gmail.com>
2022-01-07 16:56:27 -0800

    setting up stripe

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ import Shop from './pages/Shop'
 import ProductPage from './pages/ProductPage'
 import Footer from './components/Footer';
 
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
 function App() {
+
+  const stripePromise = loadStripe('pk_test_51KFT3GHOSFoUSMnACcRtY2l3R36EUZpVqJsUUixh7TSScEsWPv2StMffXAMlUQKWJaNPuUA6wDFj3STUh9mlCNDK00qo8fflqr');
+
+  const options = {
+    clientSecret: '{{CLIENT_SECRET}}',
+  };
   return (
     <div className="App">
       <Navbar/>
-      {/* <Button>button</Button>
-      <Button variant='secondary'>secondary</Button> */}
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/our-story' component={OurStory}/>
         <Route exact path='/products' component={Shop}/>
         <Route path='/products/:id' component={ProductPage}/>
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutForm />
+        </Elements>
       </Switch>
       <Footer/>
     </div>
